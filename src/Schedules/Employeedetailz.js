630e872f4f98d5e638fa0760bb0b7e9afe47ebb8
Eneojo <amehsunday078@gmail.com>
2025-09-08 12:07:28 +0100

    update

--- src/Schedules/Employeedetailz.js
+++ src/Schedules/Employeedetailz.js
@@ -1,9 +1,126 @@
 import React from 'react'
+// import { IoMdTime } from "react-icons/io";
+import { SlCalender } from "react-icons/sl";
+import { HiPlusSm } from "react-icons/hi";
 
 function Employeedetailz() {
   return (
     <div>
-        
+      <div className='shifts'>
+        <div className='shift-one'>
+
+        </div>
+        <div className='shift-one'>
+
+        </div>
+        <div className='shift-one'>
+
+        </div>
+        <div className='shift-one'>
+
+        </div>
+      </div>
+      <br />
+      <div className='shift-templates'>
+        <div className='tsf'>
+          <div className='temp-shift-flex'>
+            <div className='teem'>
+              <div className='teem-icon'>
+                <SlCalender />
+              </div>
+              <div>
+                <h3>Employee Schedules</h3>
+              </div>
+            </div>
+            <div className='tems'>
+              <div className='tem-input'>
+                <input type='text' placeholder='Search employees...'></input>
+              </div>
+              <div className='select-tem'>
+                <select>
+                  <option><h4>All Departments</h4></option>
+                  <option>Production</option>
+                  <option>Administration</option>
+                  <option>Security</option>
+                  <option>Maintenance</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          {/* Table beneath */}
+          <table className="shift">
+            <thead>
+              <tr>
+                <th>Employee</th>
+                <th>Department</th>
+                <th>Current Shift</th>
+                <th>Shift Time</th>
+                <th>Weekly<br /> Hours</th>
+                <th>Status</th>
+                <th>Next Shift</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><span className='j' style={{ fontWeight: '500' }}>John Smith</span> <br /> Machine <br />Operator</td>
+                <td>Production</td>
+                <td>Production</td>
+                <td>06:00 - <br /> 14:00</td>
+                <td>40h</td>
+                <td></td>
+                <td>Tomorrow<br /> 06:00</td>
+                <td>Active</td>
+              </tr>
+              <tr>
+                <td><span className='j' style={{ fontWeight: '500' }}>John Smith</span> <br /> Machine <br />Operator</td>
+                <td>Administration</td>
+                <td>Production</td>
+                <td>06:00 - <br /> 14:00</td>
+                <td>40h</td>
+                <td></td>
+                <td>Tomorrow<br /> 06:00</td>
+                <td>Active</td>
+              </tr>
+              <tr>
+                <td><span className='j' style={{ fontWeight: '500' }}>John Smith</span> <br /> Machine <br />Operator</td>
+                <td>Security</td>
+                <td>Night Security</td>
+                <td>06:00 - <br /> 14:00</td>
+                <td>40h</td>
+                <td></td>
+                <td>Tomorrow<br /> 06:00</td>
+                <td>Active</td>
+              </tr>
+              <tr>
+                <td><span className='j' style={{ fontWeight: '500' }}>Emily Brown</span> <br /> Machine <br />Operator</td>
+                <td>Production</td>
+                <td>Maintenance</td>
+                <td>06:00 - <br /> 14:00</td>
+                <td>40h</td>
+                <td></td>
+                <td>Tomorrow<br /> 06:00</td>
+                <td>Active</td>
+              </tr>
+              <tr>
+                <td><span className='j' style={{ fontWeight: '500' }}>David Wilson</span> <br /> Technician</td>
+                <td>Maintenance</td>
+                <td>Administration</td>
+                <td>06:00 - <br /> 14:00</td>
+                <td>40h</td>
+                <td></td>
+                <td>Tomorrow<br /> 06:00</td>
+                <td>Active</td>
+              </tr>
+            </tbody>
+          </table>
+          <br />
+        </div>
+
+
+      </div>
+      <br />
     </div>
   )
 }
--- src/Schedules/Shiftdetails.js
+++ src/Schedules/Shiftdetails.js
@@ -1,9 +1,116 @@
 import React from 'react'
+import { IoMdTime } from "react-icons/io";
+import { HiPlusSm } from "react-icons/hi";
 
 function Shiftdetails() {
   return (
     <div>
-       Hello...
+      <div className='shifts'>
+        <div className='shift-one'>
+
+        </div>
+        <div className='shift-one'>
+
+        </div>
+        <div className='shift-one'>
+
+        </div>
+        <div className='shift-one'>
+
+        </div>
+      </div>
+      <br />
+      <div className='shift-templates'>
+        <div className='tsf'>
+          <div className='temp-shift-flex'>
+            <div className='teem'>
+              <div className='teem-icon'>
+                <IoMdTime />
+              </div>
+              <div>
+                <h3>Shift Templates</h3>
+              </div>
+            </div>
+            <div className='te-em-bg'>
+              <div className='te-em'>
+                <div className='teem--icon'>
+                  <HiPlusSm />
+                </div>
+                <div>
+                  <h4>Create Shift</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Table beneath */}
+          <table className="shift">
+            <thead>
+              <tr>
+                <th>Shift Name</th>
+                <th>Time</th>
+                <th>Department</th>
+                <th>Location</th>
+                <th>Staffing</th>
+                <th>Days</th>
+                <th>Status</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Morning<br /> Production</td>
+                <td>08:00 <br /> - <br /> 12:00</td>
+                <td>Production</td>
+                <td>Factory <br /> Floor A</td>
+                <td>12/15</td>
+                <td></td>
+                <td>Active</td>
+              </tr>
+              <tr>
+                <td>Morning<br /> Production</td>
+                <td>08:00 <br /> - <br /> 12:00</td>
+                <td>Production</td>
+                <td>Factory <br /> Floor A</td>
+                <td>12/15</td>
+                <td></td>
+                <td>Active</td>
+              </tr>
+              <tr>
+                <td>Morning<br /> Production</td>
+                <td>08:00 <br /> - <br /> 12:00</td>
+                <td>Security</td>
+                <td>Main <br /> Building </td>
+                <td>12/15</td>
+                <td></td>
+                <td>Active</td>
+              </tr>
+              <tr>
+                <td>Morning<br /> Production</td>
+                <td>08:00 <br /> - <br /> 12:00</td>
+                <td>Maintenance</td>
+                <td>All <br /> Areas </td>
+                <td>12/15</td>
+                <td></td>
+                <td>Active</td>
+              </tr>
+              <tr>
+                <td>Morning<br /> Production</td>
+                <td>08:00 <br /> - <br /> 12:00</td>
+                <td>Administration</td>
+                <td>Office <br /> Building</td>
+                <td>12/15</td>
+                <td></td>
+                <td>Active</td>
+              </tr>
+            </tbody>
+          </table>
+          <br/>
+        </div>
+        
+
+      </div>
+      <br/>
     </div>
   )
 }
