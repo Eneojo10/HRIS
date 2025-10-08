9302d821676dd15c6a5352f00c71f1520b7de4ac
Eneojo <amehsunday078@gmail.com>
2025-10-08 10:44:17 +0100

    update

--- src/Schedules/Scheduledetails.js
+++ src/Schedules/Scheduledetails.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { MdAccessTime } from "react-icons/md";
+import { IoLocationOutline } from "react-icons/io5";
+import { BsPeopleFill } from "react-icons/bs";
 
 function Scheduledetails() {
     return (
@@ -54,10 +57,10 @@ function Scheduledetails() {
                                 <div className='m1-border'>
 
                                 </div>
-                                <div className='m1-border'>
+                                <div className='m1--border'>
 
                                 </div>
-                                <div className='m1-border'>
+                                <div className='m1---border'>
 
                                 </div>
                                 <div className='m1-border'>
@@ -127,32 +130,185 @@ function Scheduledetails() {
 
 
                 </div>
-                
+
                 <div className='schedule-table-border'>
                     <div className='today-schedule'>
-                        <div>
-                            <h3>Today's Schedule Overview</h3>
-                        </div><br />
+                        <div className='t-schedule'style={{display:'flex',gap:'10px'}}>
+                            <div>
+                                <MdAccessTime style={{ fontSize: '25px'}} />
+                            </div>
+                            <div>
+                                <h3>Today's Schedule Overview</h3>
+                            </div></div><br />
                         <div className='today_cards'>
                             <div className='today-cards'>
+                                <div className='morn'style={{padding:'15px'}}>
+                                    <div className='morn-staff'style={{display:'flex',justifyContent:'space-between'}}>
+                                        <div>
+                                            <p>Morning Shift</p>
+                                        </div>
+                                        <div className='ten-staff'>
+                                            <p>10 staff</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <MdAccessTime/>
+                                        </div>
+                                        <div>
+                                            <p>06:00 - 14:00</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <BsPeopleFill/>
+                                        </div>
+                                        <div>
+                                            <p>Production</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <IoLocationOutline/>
+                                        </div>
+                                        <div>
+                                            <p>Factory Floor</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button className='morn-btn'>View Details</button>
+                                    </div>
+                                </div>
 
                             </div>
                             <div className='today-cards'>
+                                <div className='morn'style={{padding:'15px'}}>
+                                    <div className='morn-staff'style={{display:'flex',justifyContent:'space-between'}}>
+                                        <div>
+                                            <p>Afternoon Shift</p>
+                                        </div>
+                                        <div className='ten--staff'>
+                                            <p>10 staff</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <MdAccessTime/>
+                                        </div>
+                                        <div>
+                                            <p>06:00 - 14:00</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <BsPeopleFill/>
+                                        </div>
+                                        <div>
+                                            <p>Production</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <IoLocationOutline/>
+                                        </div>
+                                        <div>
+                                            <p>Factory Floor</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button className='morn-btn'>View Details</button>
+                                    </div>
+                                </div>
 
                             </div>
                             <div className='today-cards'>
+                                <div className='morn'style={{padding:'15px'}}>
+                                    <div className='morn-staff'style={{display:'flex',justifyContent:'space-between'}}>
+                                        <div>
+                                            <p>Night Shift</p>
+                                        </div>
+                                        <div className='ten---staff'>
+                                            <p>10 staff</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <MdAccessTime/>
+                                        </div>
+                                        <div>
+                                            <p>06:00 - 14:00</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <BsPeopleFill/>
+                                        </div>
+                                        <div>
+                                            <p>Security</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <IoLocationOutline/>
+                                        </div>
+                                        <div>
+                                            <p>Main Building</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button className='morn-btn'>View Details</button>
+                                    </div>
+                                </div>
 
                             </div>
                             <div className='today-cards'>
+                                <div className='morn'style={{padding:'15px'}}>
+                                    <div className='morn-staff'style={{display:'flex',justifyContent:'space-between'}}>
+                                        <div>
+                                            <p>Office Hours</p>
+                                        </div>
+                                        <div className='ten----staff'>
+                                            <p>45 staff</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <MdAccessTime/>
+                                        </div>
+                                        <div>
+                                            <p>06:00 - 14:00</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <BsPeopleFill/>
+                                        </div>
+                                        <div>
+                                            <p>Administration</p>
+                                        </div>
+                                    </div>
+                                    <div className='ms'style={{display:'flex',marginTop:'10px',gap:'6px',color:'#82828a'}}>
+                                        <div className='morn-icon'style={{marginTop:'1px'}}>
+                                            <IoLocationOutline/>
+                                        </div>
+                                        <div>
+                                            <p>Office Building</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <button className='morn-btn'>View Details</button>
+                                    </div>
+                                </div>
 
                             </div>
+                            
                         </div>
                     </div>
-                    <br/>
+                    <br />
 
                 </div>
             </div>
-            <br/>
+            <br />
 
         </div>
     )
