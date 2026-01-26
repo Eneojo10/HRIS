d04eebf2902c7aa5b68a671aac8facab37151400
Eneojo <amehsunday078@gmail.com>
2026-01-26 23:03:10 +0100

    button

--- src/Pages/Utils/globals.js
+++ src/Pages/Utils/globals.js
@@ -1,3 +1,2 @@
 // src/Pages/Utils/globals.js
-
-export const BASE_URL = 'https://human-resource-backend-a3xn.onrender.com';
+export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
